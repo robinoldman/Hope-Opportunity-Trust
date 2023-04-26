137746d9ce8b928a824915003130905ed3b00805
robinoldman <>
2023-04-26 11:56:59 +0000

    fix sicky navbar bug

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('.nav')
 
 window.addEventListener('scroll', fixNav)
 
-function fixnav() {
+function fixNav() {
     if(window.scrollY > nav.offsetHeight + 150)
     {   nav.classList.add('active')}
     else{
