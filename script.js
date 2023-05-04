3166aee9eb05154c47f55da749ba4bd22257097e
robinoldman <>
2023-05-04 16:34:16 +0000

    add graphics and movement to active class in java

--- script.js
+++ script.js
@@ -28,3 +28,19 @@ valueDisplays.forEach((valueDisplays) => {
     }, duration);
 
 });
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panel.forEach(panel => {
+        panel.classList.remove('active')
+    })
+
+}
