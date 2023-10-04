419e4e162834ac3a6f5b074c7ec2a5180ec49a9d
robinoldman <robin.c.oldman@gmail.com>
2023-10-04 21:07:04 +0000

    add additional text to school section and corrrected typos in other pages

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150)
-    {   nav.classList.add('active')}
-    else{
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
         nav.classList.remove('active')
     }
 
@@ -16,13 +16,12 @@ let interval = 5000;
 
 valueDisplays.forEach((valueDisplays) => {
     let startValue = 0;
-    let endValue = parseInt(valueDisplays.getAttribute
-    ('data-val'));
+    let endValue = parseInt(valueDisplays.getAttribute('data-val'));
     let duration = Math.floor(interval / endValue);
-    let counter = setInterval (function(){
+    let counter = setInterval(function () {
         startValue += 1;
         valueDisplays.textContent = startValue;
-        if (startValue == endValue){
+        if (startValue == endValue) {
             clearInterval(counter);
         }
     }, duration);
